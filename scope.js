c38f9f65ae7042faf169d960ad6ccdf233bb4190
gonzalez-7 <20agonzalezp@gmail.com>
2024-10-30 20:33:13 -0700

    g

---
+++ scope.js
@@ -0,0 +1,10 @@
+let name = "Jonathan"; // this variable is available in the entire program
+
+if (name == "Jonathan") {
+    name = "Cruz";
+    let helloStatement =  "Hello World!";
+    console.log(helloStatement);
+}
+
+
+console.log(name);
